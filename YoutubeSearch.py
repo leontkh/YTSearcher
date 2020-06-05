ada1fa518c067c15c97c9b0e60953ea4fe8b7ce9
leontkokhow <60612034+leontkokhow@users.noreply.github.com>
2020-06-05 22:19:23 +0800

    Update YoutubeSearch.py

--- YoutubeSearch.py
+++ YoutubeSearch.py
@@ -1,30 +1,25 @@
-api_key = "AIzaSyB6vdnr4AOr2RN9rJx6VSaT_7LxDQliMBg"
+api_key = "KSNAKFKANKKKVIFNWodgkj_sN-xyiNy0Vrq4YeY" #Please input your own API key here
 import webbrowser
 from apiclient.discovery import build
+#setting up to access youtube through its API
 youtube = build('youtube', 'v3' , developerKey=api_key)
 
 
-#    request = youtube.playlists().insert(
-#        part="snippet",
-#        body={
-#          "snippet": {
-#            "title": "PyTube Playlist"
-#          }
-#        }
-#    )
-#    response = request.execute()
 def main():
-    
+    #variables needed to loop the following questions below
     ys_continue = True
     while ys_continue:
         content_check = True
         view_on = True
         ask_on = True
         
+        #First question
         while content_check:
             content_type = input("Are you looking for [V]ideos or [C]hannels?")
             if content_type.lower() == "v" or content_type.lower() == "c":
                 content_check = False
+                
+        #Second question if user wants videos
         if content_type == "v":
             query = input("What are you searching for?")
             req = youtube.search().list(q=query , part='snippet', type="video", maxResults=10)
@@ -32,9 +27,13 @@ def main():
             for i in range(0,len(res['items'])):
                 print(i+1,". ",res['items'][i]['snippet']['title'])
             while view_on:
+                
+                #Third question
                 view = int(input("Which one would you be interested in? [1-10]"))
                 yt_website = "https://www.youtube.com/watch?v="+res['items'][view-1]['id']['videoId']
                 webbrowser.open_new_tab(yt_website)
+                
+                #Ask user if user wants to repeat
                 while ask_on:
                     repeat = input("Continue? [Y] or [N]")
                     if repeat.lower() == ("y"):
@@ -45,16 +44,21 @@ def main():
                         ask_on = False
                         ys_continue = False
 
+        #Second question if user wants channels
         elif content_type == "c":
             query = input("What are you searching for?")
             req = youtube.search().list(q=query , part='snippet', type="channel", maxResults=10)
             res = req.execute()
             for i in range(0,len(res['items'])):
                 print(i+1,". ",res['items'][i]['snippet']['channelTitle'])
+                
+            #Third question
             while view_on:
                 view = int(input("Which one would you be interested in? [1-10]"))
                 yt_website = "https://www.youtube.com/channel/"+res['items'][view-1]['id']['channelId']
                 webbrowser.open_new_tab(yt_website)
+                
+                #Ask user if user wants to repeat
                 while ask_on:
                     repeat = input("Continue? [Y] or [N]")
                     if repeat.lower() == ("y"):
